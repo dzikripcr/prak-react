6ed7ed249ddf8d4cfa5d3542a041f25802800459
dzikripcr <dzikri24si@mahasiswa.pcr.ac.id>
2026-04-07 15:28:17 +0700

    seacrh dan filter data - P4

--- src/pertemuan-4/FrameworkListSearchFilter.jsx
+++ src/pertemuan-4/FrameworkListSearchFilter.jsx
@@ -1,11 +1,58 @@
+import { useState } from "react";
 import frameworkData from "./framework.json";
+useState;
 
-export default function FrameworkList() {
+export default function FrameworkListSeacrhFilter() {
+  /** Deklrasai state **/
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedTag, setSelectedTag] = useState("");
+
+  /** Deklrasai Logic Search & Filter **/
+  const _searchTerm = searchTerm.toLowerCase();
+  const filteredFrameworks = frameworkData.filter((framework) => {
+    const matchesSearch =
+      framework.name.toLowerCase().includes(_searchTerm) ||
+      framework.description.toLowerCase().includes(_searchTerm) ||
+      framework.details.developer.toLowerCase().includes(_searchTerm) ||
+      framework.details.releaseYear.toString().includes(_searchTerm);
+
+    const matchesTag = selectedTag
+      ? framework.tags.includes(selectedTag)
+      : true;
+
+    return matchesSearch && matchesTag;
+  });
+
+  /** Deklarasi pengambilan unique tags di frameworkData **/
+  const allTags = [
+    ...new Set(frameworkData.flatMap((framework) => framework.tags)),
+  ];
   return (
     <div className="p-8 bg-gray-50 min-h-screen">
       {/* Container untuk mengatur grid agar tidak menumpuk satu kolom saja di layar lebar */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {frameworkData.map((item) => (
+        <input
+          type="text"
+          name="searchTerm"
+          placeholder="Search framework..."
+          onChange={(e)=>setSearchTerm(e.target.value)}
+          className="w-full p-2 border border-gray-300 rounded mb-4"
+        />
+
+        <select
+          name="selectedTag"
+          className="w-full p-2 border border-gray-300 rounded mb-4"
+          onChange={(e)=>setSelectedTag(e.target.value)}
+        >
+          <option value="">All Tags</option>
+          {allTags.map((tag, index) => (
+            <option key={index} value={tag}>
+              {tag}
+            </option>
+          ))}
+        </select>
+
+        {filteredFrameworks.map((item) => (
           <div
             key={item.id}
             className="group border border-gray-200 p-6 rounded-2xl shadow-sm bg-white 
--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./tailwind.css"
 import FrameworkList from "./FrameworkList";
+import FrameworkListSeacrhFilter from "./FrameworkListSearchFilter";
 
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-            <FrameworkList/>
+            {/* <FrameworkList/> */}
+            <FrameworkListSeacrhFilter/>
         </div>
     )
