0b5f6c7ffed1a7643679d0b54f4d0e48b098de4c
dzikripcr <dzikri24si@mahasiswa.pcr.ac.id>
2026-04-28 14:46:47 +0700

    Membuat Multi Layout & Nested Routes - P7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import "./assets/tailwind.css";
-import Sidebar from "./layouts/SideBar";
-import Header from "./layouts/Header";
 import Dashboard from "./pages/Dashboard";
 import Orders from "./pages/Orders";
 import Customers from "./pages/Customers";
@@ -10,30 +8,36 @@ import NotFound from "./pages/NotFound";
 import Error400 from "./pages/error400";
 import Error401 from "./pages/error401";
 import Error403 from "./pages/error403";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div id="app-container" className="bg-gray-100 min-h-screen flex">
-        <div id="layout-wrapper" className="flex flex-row flex-1">
-          <Sidebar />
-          <div id="main-content" className="flex-1 p-4">
-            <Header />
-            <Routes>
-              <Route path="*" element={<NotFound />} />
-              {/* Error Pages */} 
-              <Route path="/error400" element={<Error400 />} />
-              <Route path="/error401" element={<Error401 />} />
-              <Route path="/error403" element={<Error403 />} />
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/customers" element={<Customers />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </>
+    <Routes>
+      {/* Route MainLayout */}
+      <Route element={<MainLayout />}>
+        {/* Error Pages */}
+        <Route path="*" element={<NotFound />} />
+        <Route path="/error400" element={<Error400 />} />
+        <Route path="/error401" element={<Error401 />} />
+        <Route path="/error403" element={<Error403 />} />
+
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/customers" element={<Customers />} />
+      </Route>
+
+      {/* Route AuthLayout */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+      </Route>
+    </Routes>
   );
 }
